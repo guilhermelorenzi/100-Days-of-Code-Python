047dc675ecbe9a26f310bc247223ad686cb9b5e0
Guilherme Lorenzi <guilorenzi@gmail.com>
2024-11-28 21:36:02 -0300

    Day 2

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,18 @@
-len("Hello")
+#subscripting
+print("Hello"[0])
+
+#string
+print("123" + "332")
+
+#integer
+print(123 + 332)
+
+#large integers
+print(123_345_234)
+
+#float
+print(3.13159)
+
+#boolean
+print(True)
+print(False)
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,17 @@
-len(12345)
+print(type("Hello"))
+print(type(123))
+print(type(123.332))
+print(type(True))
+
+print("123" + "345")
+int("123")
+print(int("123") + int("345"))
+
+
+name_user = input("Enter your name")
+length_name = len(name_user)
+
+print(type("Number of letters in your name: "))
+print(type(length_name))
+
+print("Number of letters in your name: " + str(length_name))
