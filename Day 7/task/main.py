f9b7c0b22003cb2719ba5667c89c5bfdc27b0f2c
guilhermelorenzi <guilorenzi@gmail.com>
2024-12-26 17:33:36 -0300

    Day 7 - steps 3 (incomplete)

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -4,25 +4,31 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-
 placeholder = ""
-
-for position in chosen_word:
+word_length = len(chosen_word)
+for position in range(word_length):
     placeholder += "_"
-
 print(placeholder)
 
-guess = input("Guess a letter: ").lower()
+# TODO-1: - Use a while loop to let the use guess again.
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+word_blanks = word_length
 
-display = ""
+while word_blanks != 0:
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    display = ""
+
+# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            word_blanks -= 1
+        else:
+            display += "_"
+
+    print(display)
+    print(word_length)
+    print(word_blanks)
